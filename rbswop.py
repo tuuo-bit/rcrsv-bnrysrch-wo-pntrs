3622e509d245222c2ee1b9da5013b6fe6f7febcc
tuuo-bit <142587159+tuuo-bit@users.noreply.github.com>
2024-03-04 20:11:35 +0530

    Update rbswop.py

--- rbswop.py
+++ rbswop.py
@@ -1,15 +1,18 @@
-def binarySearch( arr, ele, index=0):
+# Recurs_ve b_nary search w_thout po_nters (only time _'ll be using i&j)
+# to return _ndex of element from (None _n case not from) a sorted array.
+
+def b_narySearch( arr, ele, _ndex=0):
     if len( arr) == 1 and arr[ 0] != ele:
         return
     elif ele in arr:
-        mid = len( arr) // 2
-        if arr[ mid] == ele:
-            index += mid
-            return index
-        elif arr[ mid] > ele:
-            return binarySearch( arr[:mid], ele, index)
-        elif arr[ mid] < ele:
-            index += mid
-            return binarySearch( arr[mid:], ele, index)
+        m_d = len( arr) // 2
+        if arr[ m_d] == ele:
+            _ndex += m_d
+            return _ndex
+        elif arr[ m_d] > ele:
+            return b_narySearch( arr[:m_d], ele, _ndex)
+        elif arr[ m_d] < ele:
+            _ndex += m_d
+            return b_narySearch( arr[m_d:], ele, _ndex)
     else:
         return
